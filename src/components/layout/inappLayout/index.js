e23e1902f15cc3fa9a4c022ae9a46dd91a068ced
tegadaniel <tegadaniels8@gmail.com>
2022-09-22 00:16:23 +0100

    final commit

--- src/components/layout/inappLayout/index.js
+++ src/components/layout/inappLayout/index.js
@@ -6,18 +6,18 @@ import { motion } from "framer-motion";
 
 export default function InAppLayout() {
   return (
-    <div className="grid grid-cols-12 h-screen w-full grid-rows-10">
-      <nav className=" col-start-1 col-span-2 border-r  min-h-[100vh] space-x-5 xl:w-[220px] md:w-[100px] w-20  px-5 xl:flex bg-[#031434]">
+    <div className="grid xl:grid-cols-12 md:grid-cols-16 grid-cols-16 h-screen w-full xl:grid-rows-10">
+      <nav className=" xl:col-start-1 xl:col-span-2 border-r  min-h-[100vh] space-x-5 xl:w-[210px] md:w-[100px] w-20  px-5 xl:flex bg-[#031434] md:col-start-1 md:col-end-1">
         <Sidebar />
       </nav>
-      <header className="bg-BACKGROUND_WHITE p-6 xl:w-[86%] md:w-[95%] w-full fixed top-0 z-50 h-16 shadow-header mx-20 xl:mx-56 md:mx-[50]">
+      <header className="bg-BACKGROUND_WHITE p-6 xl:w-[86%] md:w-[90%] w-full fixed top-0 z-50 h-16 shadow-header mx-20 xl:mx-56 md:mx-[50]">
         <Header />
       </header>
       <motion.div
         initial={{ opacity: 0, y: 40 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ ease: "easeIn", duration: 0.8 }}
-        className="bg-[#FFFFFF] md:p-5 xl:col-start-3 xl:col-end-13 p-3 mt-16 col-start-4 col-end-13"
+        className="bg-[#FFFFFF] md:p-5 xl:col-start-3 xl:col-end-13 p-3 mt-16 col-start-4 col-span-full md:col-start-3 md:col-span-full space-x-11"
       >
         <Outlet />
       </motion.div>
